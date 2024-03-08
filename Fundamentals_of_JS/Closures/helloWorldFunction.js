422d1b227db2d16d8a04c24d05bacf3adb33af1e
Ariha Noor <arihanoor0@gmail.com>
2024-03-09 00:24:48 +0500

    initial-commit

---
+++ Fundamentals_of_JS/Closures/helloWorldFunction.js
@@ -0,0 +1,16 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        var str = "Hello World";
+        return str;
+    }
+};
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
